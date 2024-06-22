d10593e671b0f403ec6ab5479b1ca7a0d8938f48
KimTaeYoung0589 <m45416820@gmail.com>
2024-06-23 02:14:52 +0900

    feat: use mui - make login ui

--- use_mui/src/App.js
+++ use_mui/src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextFiled from "@mui/material/TextField";
+import { Avatar, Box, Checkbox, Container } from "@mui/material";
+import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import LockOutlinedIcon from "@mui/icons-material/LockClockOutlined";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon fontSize="large" />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+
+        <TextFiled
+          label="Email Address"
+          name="email"
+          required
+          fullWidth
+          autoComplete="email"
+          autoFocus
+          sx={{ mt: 3, mb: 2 }}
+        />
+
+        <TextFiled
+          label="Password"
+          type="password"
+          name="password"
+          fullWidth
+          required
+          autoComplete="current-password"
+        />
+
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+          sx={{
+            "& .MuiFormControlLabel-label": {
+              fontWeight: "bold",
+            },
+          }}
+        />
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Sign in
+        </Button>
+
+        <Grid container>
+          <Grid item xs sx={{ mt: 1 }}>
+            <Link>Forgot password?</Link>
+          </Grid>
+          <Grid item sx={{ mt: 1 }}>
+            <Link>Sign Up</Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 }
 
